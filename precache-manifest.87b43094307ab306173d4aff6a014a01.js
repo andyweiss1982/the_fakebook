0937073f5667750cb749824de4991ce233688e4d
Andy Weiss <andyweiss1982@gmail.com>
2019-06-24 14:41:27 -0400

    Updates

---
+++ precache-manifest.87b43094307ab306173d4aff6a014a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9f1fd4001858ebe1c42f3ef16fc5b8",
+    "url": "/the_fakebook/index.html"
+  },
+  {
+    "revision": "7169ebc741fa0d184a6a",
+    "url": "/the_fakebook/static/css/main.2fd69b80.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/the_fakebook/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "7169ebc741fa0d184a6a",
+    "url": "/the_fakebook/static/js/main.51388840.chunk.js"
+  },
+  {
+    "revision": "f4f6893ba4079bcd3080",
+    "url": "/the_fakebook/static/js/runtime~main.2da3b4a3.js"
+  }
+]);
